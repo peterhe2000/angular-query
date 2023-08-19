d5d5ba1597cef4b9678b5bd9beb6a667ddd61cc0
Peter He <peter.he@xakiatech.com>
2023-08-19 11:25:21 +1000

    Got dev tools working

--- angular-query/apps/angular-query/src/app/app.module.ts
+++ angular-query/apps/angular-query/src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import {
+  ENVIRONMENT_INITIALIZER,
+  inject,
+  NgModule
+} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { QueryClientService } from '@ngneat/query';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -15,6 +20,16 @@ import { UserContainerComponent } from './pages/user/user-container.component';
     {
       provide: API_BASE_URL,
       useValue: environment.api_base_url,
+    },
+    {
+      provide: ENVIRONMENT_INITIALIZER,
+      multi: true,
+      useValue() {
+        const queryClient = inject(QueryClientService);
+        import('@ngneat/query-devtools').then((m) => {
+          m.ngQueryDevtools({ queryClient });
+        });
+      },
     },
   ],
   bootstrap: [AppComponent],
